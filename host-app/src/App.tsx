f7645c3044a85f199c8981393eb57474507c78fd
Danil <dmtrvd@Danils-MacBook-Pro.local>
2025-01-25 18:06:33 +0300

    add settings ts

--- host-app/src/App.tsx
+++ host-app/src/App.tsx
@@ -1,19 +1,24 @@
-import { useState } from "react";
-import { List } from "todo_components/List";
-import { Input } from "todo_components/Input";
+// import { useState } from "react";
+// import { List } from "todo_components/List";
+// import { Input } from "todo_components/Input";
 
 function App() {
-  const [newTodo, setNewTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-  const onSubmit = () => {
-    setTodos((prev) => [...prev, newTodo]);
-    setNewTodo("");
-  };
+  // const [newTodo, setNewTodo] = useState("");
+
+  // const [todos, setTodos] = useState([]);
+
+  // const onSubmit = () => {
+  //   setTodos((prev) => [...prev, newTodo]);
+  //   setNewTodo("");
+  // };
 
   return (
     <>
-      <Input value={newTodo} onChange={setNewTodo} onSubmit={onSubmit} />
-      <List items={todos} />
+      {/* <Input value={newTodo} onChange={setNewTodo} onSubmit={onSubmit} />
+      <List items={todos} /> */}
+
+      {/* <Input />
+      <List /> */}
     </>
   );
 }
